1a393165726c686b2f9753703b187c0f236c8034
evry1lovesjay <evry1lovesjay@yahoo.com>
2023-05-10 11:29:25 +0100

    changed from localhost to netlify url

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const connect = async () => {
     }
 }
 
-app.use(cors({origin:"http://localhost:5173", credentials:true}))
+app.use(cors({origin:"https://reactfiverr.netlify.app", credentials:true}))
 // app.use(cors({origin:"http://localhost:5173", credentials:true}))
 app.use(express.json())
 app.use(cookieParser())
